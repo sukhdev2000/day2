c237c1a38545169af0164b1ff0dfb54f1edb9683
sukhdev2000[ <dsukhdev2000@gmail.com>
2021-08-17 20:44:41 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        //process of converting value type to reference type is called boxing
+        //process of converting reference type to value type is called unboxing
+        static void Main(string[] args)
+        {
+        int i=100;
+        object obj=i;//boxing
+        int x=(int)obj;//unboxing 
+        System.Console.WriteLine(i);
+        System.Console.WriteLine(obj);
+        System.Console.WriteLine(x);
+        }
+    }
+}
